b657648cb09779a6765893c9bede2890768dcf6a
Yuliia Ustymenko <ustimenkoiuliia@gmail.com>
2023-03-08 01:40:32 +0200

    finished calculator, need refactor

--- file.js
+++ file.js
@@ -1,38 +1,86 @@
-let result = document.querySelector('#result');
-let expression = document.querySelector('#expression')
+let number1 = '';
+let number2 = '';
+let operator = '';
+let finish = false;
+const screen = document.querySelector('.screen')
 
-let buttons = document.querySelectorAll('.btn');
-for (let button of buttons) {
-  button.addEventListener('click', showOnScreen)
+let numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+let operators = ['+', '-', '/', 'X', '%'];
+
+
+let btnReset = document.querySelector('.btn-ac');
+btnReset.addEventListener('click', reset);
+
+function reset() {
+   number1 = '';
+   number2 = '';
+   operator = '';
+   finish = false;
+  screen.textContent = 0;
 }
 
-
-
-
-function calc(number1, number2, operator) {
-  let result;
-  switch (operator) {
-    case '+':
-      result = number1 + number2;
-      break;
-    case '-':
-      result = number1 + number2;
-      break;
-    case '*':
-      result = number1 * number2;
-      break;
-    case '/':
-      result = number1 / number2;
-    case '%':
-      result = percentage();
-  }
-}
-
-function percentage(number1, number2) {
+function percentage() {
   return (number1 / 100) * number2;
 }
 
-function showOnScreen(e) {
- expression.innerHtml = e.target.textContent;
+let buttons = document.querySelectorAll('.btn');
+for (let button of buttons) {
+  button.addEventListener('click', function (e) {
+    const value = button.textContent;
+    
+    if (numbers.includes(value)) {
+      if (number2 === '' && operator === '') {
+        number1 += value;
+        screen.textContent = number1;
+      } else if (number1 !== '' && number2 !== '' && finish) {  
+        number2 = value;
+        finish = false;
+        screen.textContent = number1 + operator + number2;
+      } else {
+        number2 += value;
+        screen.textContent = number1 + operator + number2;
+
+      }
+      console.log(number1, number2, operator)
+      return;
+    }
+    if (operators.includes(value)) {
+      operator = value;
+      screen.textContent = number1 + operator;
+      console.log(number1, number2, operator)
+      return;
+    }
+
+    if (value === '=') {
+      if (number2 === '') number2 = number1;
+      switch (operator) {
+        case '+':
+          number1 = (+number1) + (+number2);
+          break;
+        case '-':
+          number1 = number1 - number2;
+          break;
+        case 'X':
+          number1 = number1 * number2;
+          break;
+        case '/':
+          if (number2 === '0') {
+            screen.textContent = 'Error';
+            number1 = '';
+            number2 = '';
+            operator = '';
+            return;
+          }
+          number1 = number1 / number2;
+          break;
+        case '%':
+          number1 = percentage(number1, number2);
+          break;   
+      }
+      finish = true;
+      screen.textContent = number1;
+      console.log(number1, number2, operator)
+    }
+  })
 }
 
